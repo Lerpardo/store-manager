7b0a61cfa81605b0741e6402a0ac5fe0fc8eb99a
Lerpardo <ls6182315@gmail.com>
2023-01-22 00:41:29 -0300

    products models

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -2,12 +2,21 @@
 const connection = require('./connection');
 
 const findAll = async () => {
-    const [result] = await connection.execute(
-        'SELECT * FROM  StoreManager.products',
-    );
-    return result;
+  const [result] = await connection.execute(
+    'SELECT * FROM  StoreManager.products',
+  );
+  return result;
+};
+
+const findById = async (productId) => {
+  const [result] = await connection.execute(
+    'SELECT * FROM  StoreManager.products WHERE id = ?',
+    [productId],
+  );
+  return result;
 };
 
 module.exports = {
   findAll,
+  findById,
 };
