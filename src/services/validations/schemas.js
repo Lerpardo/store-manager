20314ef236d49b7954c1a32757e9ec8d43b4fd94
Lerpardo <ls6182315@gmail.com>
2023-01-24 08:01:22 -0300

    new product validations

--- src/services/validations/schemas.js
+++ src/services/validations/schemas.js
@@ -2,6 +2,13 @@ const Joi = require('joi');
 
 const idSchema = Joi.number().integer().min(1).required();
 
+const pointSchema = Joi.string().min(3).required();
+
+const addProductSchema = Joi.object({
+  name: pointSchema,
+});
+
 module.exports = {
   idSchema,
+  addProductSchema,
 };
