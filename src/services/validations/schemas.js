548863ecd0773bd3fff8d84c2175b09c5f2c30b1
Lerpardo <ls6182315@gmail.com>
2023-01-24 09:14:11 -0300

    product name validation

--- src/services/validations/schemas.js
+++ src/services/validations/schemas.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 const idSchema = Joi.number().integer().min(1).required();
 
-const pointSchema = Joi.string().min(3).required();
+const pointSchema = Joi.string().min(5).required();
 
 const addProductSchema = Joi.object({
   name: pointSchema,
--- src/services/validations/validationsInputValues.js
+++ src/services/validations/validationsInputValues.js
@@ -2,15 +2,15 @@ const { idSchema, addProductSchema } = require('./schemas');
 
 const validateId = (id) => {
   const { error } = idSchema.validate(id);
-  if (error) return { type: 'INVALID_VALUE', message: '"id" must be a number' };
+  if (error) return { type: 'INVALID_VALUE', message: error.message };
 
   return { type: null, message: '' };
 };
 
 const validateNewProduct = (name) => {
   const { error } = addProductSchema.validate({ name });
-  if (error) return { type: 'INVALID_VALUE', message: error.message };
-
+  if (error && name) return { type: 'INVALID_VALUE', message: error.message };
+  if (error) return { type: 'MISSING_KEY', message: error.message };
   return { type: null, message: '' };
 };
 module.exports = {
